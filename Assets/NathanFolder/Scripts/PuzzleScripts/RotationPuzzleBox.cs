aa47bc5a30c6769fe283eb30c4b7fe2ab46faac0
Altorook <nathanberry403@gmail.com>
2025-03-03 21:31:28 -0700

    Puzzle implemented in game. looked into lighting

--- Assets/NathanFolder/Scripts/PuzzleScripts/RotationPuzzleBox.cs
+++ Assets/NathanFolder/Scripts/PuzzleScripts/RotationPuzzleBox.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class RotationPuzzleBox : MonoBehaviour
+public class RotationPuzzleBox : MonoBehaviour, IInteractable
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject PuzzleCanvas;
+    public UnityEvent UIOpen;
     void Start()
     {
         
     }
-
+    public void InteractedWith()
+    {
+        PuzzleCanvas.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        UIOpen.Invoke();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PuzzleCanvas.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            UIOpen.Invoke();
+        }
     }
 }
