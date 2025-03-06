1c660b635c0d445c1406e06dfb5d256984c70239
Altorook <nathanberry403@gmail.com>
2025-03-06 15:54:59 -0700

    Tweaks

--- Assets/NathanFolder/Scripts/InteractableDoor.cs
+++ Assets/NathanFolder/Scripts/InteractableDoor.cs
@@ -1,9 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class InteractableDoor : MonoBehaviour, IInteractable
 {
     bool isOpen = false;
     bool isOpening = false;
+    float amountOpen = 0;
+    [SerializeField] float amountToOpen;
+    [SerializeField]
+    float openSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,20 +18,32 @@ public class InteractableDoor : MonoBehaviour, IInteractable
     {
         if(isOpen == false)
         {
-            float xRot = this.transform.rotation.x;
-            this.transform.Rotate(new Vector3(0,0,135));
+           
+           
             isOpen = true;
             isOpening = true;
             Debug.Log("Open?");
+            OpenDoor();
         }
     }
     public void OpenDoor()
     {
-
+        
+        if(amountOpen < amountToOpen)
+        {
+            amountOpen += openSpeed;
+            this.transform.Rotate(new Vector3(0, 0, openSpeed));
+            StartCoroutine(SmoothOpen());
+        }
+    }
+    IEnumerator SmoothOpen()
+    {
+        yield return new WaitForFixedUpdate();
+        OpenDoor();
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+       
     }
 }
