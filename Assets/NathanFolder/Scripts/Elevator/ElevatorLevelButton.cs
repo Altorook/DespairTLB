91e140cb8b34dacebf2fb193e14ab434f4d784a2
Altorook <nathanberry403@gmail.com>
2025-03-29 14:29:07 -0600

    Elevator ready to try an put into scene?

--- Assets/NathanFolder/Scripts/Elevator/ElevatorLevelButton.cs
+++ Assets/NathanFolder/Scripts/Elevator/ElevatorLevelButton.cs
@@ -5,29 +5,54 @@ using UnityEngine.Events;
 public class ElevatorLevelButton : MonoBehaviour , IInteractable
 {
     [SerializeField] GameObject LevelOfButton;
+    [SerializeField] GameObject LevelThisElevator;
     [SerializeField] bool isOnFloorOfButton;
     [SerializeField] Transform playerTransform;
     public UnityEvent OpenDesiredFloor;
     public UnityEvent CloseThisElevator;
+    Vector3 parentRelativePos;
+    Quaternion parentRelativeRot;
+    [SerializeField] bool canAccessFloor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    public void EnableFloor()
+    {
+        canAccessFloor = true;
+    }
     public void InteractedWith()
     {
-        if(!isOnFloorOfButton)
+        if(!isOnFloorOfButton && canAccessFloor)
         {
             StartCoroutine(WaitForElevator());
-            OpenDesiredFloor.Invoke();
+            
             CloseThisElevator.Invoke();
         }
     }
     IEnumerator WaitForElevator()
     {
         //play elevator noise
-        yield return new WaitForSeconds(4);
-        playerTransform.position = LevelOfButton.transform.position;
+        yield return new WaitForSeconds(7);
+        Debug.Log("Attempt Teleport");
+        // playerTransform.position = LevelOfButton.transform.position;
+        //playerTransform.SetParent(LevelOfButton.transform);
+        playerTransform.SetParent(LevelThisElevator.transform);
+        yield return new WaitForSeconds(1);
+        parentRelativePos = playerTransform.localPosition;
+        parentRelativeRot = playerTransform.localRotation;
+        
+        yield return new WaitForSeconds(1);
+        playerTransform.SetParent(LevelOfButton.transform);
+        yield return new WaitForSeconds(1);
+        playerTransform.gameObject.GetComponent<CharacterController>().enabled = false;
+        playerTransform.SetLocalPositionAndRotation(parentRelativePos, parentRelativeRot);
+        playerTransform.gameObject.GetComponent<CharacterController>().enabled = true;
+        OpenDesiredFloor.Invoke();
+        yield return new WaitForSeconds(.2f);
+        playerTransform.SetParent(null);
     }
     // Update is called once per frame
     void Update()
--- Assets/NathanFolder/Scripts/Elevator/OpenElevatorButton.cs
+++ Assets/NathanFolder/Scripts/Elevator/OpenElevatorButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OpenElevatorButton : MonoBehaviour, IInteractable
 {
@@ -16,6 +17,10 @@ public class OpenElevatorButton : MonoBehaviour, IInteractable
     float lerpTime;
     float startXRight;
     float endXRight;
+    bool isOpen;
+    bool isClose = true;
+    bool hasBeenOpened = false;
+    public UnityEvent EnableFloor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,7 +31,15 @@ public class OpenElevatorButton : MonoBehaviour, IInteractable
     }
     public void InteractedWith()
     {
-       StartCoroutine(Open());
+        if (!isOpen && !opening && !closing)
+        {
+            StartCoroutine(Open());
+            if(hasBeenOpened == false)
+            {
+                EnableFloor.Invoke();
+                hasBeenOpened = true;
+            }
+        }
     }
     IEnumerator Open()
     {
@@ -37,6 +50,8 @@ public class OpenElevatorButton : MonoBehaviour, IInteractable
         closing = false;
         opening = false;
         lerpTime = 0;
+        isOpen = true;
+        isClose = false ;
     }
     IEnumerator Close()
     {
@@ -47,28 +62,30 @@ public class OpenElevatorButton : MonoBehaviour, IInteractable
         closing = false;
         opening = false;
         lerpTime = 0;
+        isOpen = false;
+        isClose = true;
     }
     void ClosingProcess()
     {
-        Debug.Log("closing");
+     //   Debug.Log("closing");
         lerpTime += Time.deltaTime * openRate;
         LeftDoor.localPosition =new Vector3(LeftDoor.localPosition.x, Mathf.Lerp(endXLeft, startXLeft, lerpTime),LeftDoor.localPosition.z );
         RightDoor.localPosition = new Vector3(RightDoor.localPosition.x, Mathf.Lerp(endXRight, startXRight, lerpTime), RightDoor.localPosition.z);
     }
     void OpeningProcess()
     {
-        Debug.Log("opening");
+       // Debug.Log("opening");
         lerpTime += Time.deltaTime * openRate;
         LeftDoor.localPosition = new Vector3(LeftDoor.localPosition.x, Mathf.Lerp(startXLeft, endXLeft, lerpTime), LeftDoor.localPosition.z);
         RightDoor.localPosition = new Vector3(RightDoor.localPosition.x, Mathf.Lerp(startXRight, endXRight, lerpTime), RightDoor.localPosition.z);
     }
     public void EnableOpen()
     {
-        StartCoroutine(Open());
+        if( !isOpen && !opening && !closing)StartCoroutine(Open());
     }
     public void EnableClose()
     {
-        StartCoroutine(Close());
+        if(!isClose && !opening && !closing) StartCoroutine(Close());
     }
     // Update is called once per frame
     void FixedUpdate()
